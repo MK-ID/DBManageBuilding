f44e921d7e5b9e34e06e4884ca6a46dc40caa647
Luis Mikhail <luispalominopaucar@hotmail.com>
2022-05-09 23:24:26 -0500

    se crearon las tablas de personas y tipo de documento

--- DataBaseBuilding.sql
+++ DataBaseBuilding.sql
@@ -10,3 +10,29 @@ go
 ------------------------------------------------------------------------------------------------------------
 use DB_BuildingManage
 go
+
+if OBJECT_ID('TPersons','U') is not null
+	drop table  TPersons
+go 
+create table TPersons
+(
+	TPersonsID UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
+	LastName VARCHAR(255) not null,
+	FirstName VARCHAR (255) not null,
+	BirthDay DATE not null,
+	
+)  
+go
+
+if OBJECT_ID('TDocument','U') is not null
+	drop table  TDocument
+go 
+create table TDocument
+(
+	TDocument UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
+	NameDocument VARCHAR(55) not null,
+	DescriptionDocument VARCHAR(100) not null,
+	FOREIGN KEY (TPersonsID) REFERENCES TPersons
+	
+)  
+go
